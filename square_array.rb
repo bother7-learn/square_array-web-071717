41915fa4b0a011346687ceb1263ce1b69acebfe8
Joe Cha <josephmcha@gmail.com>
2017-07-07 14:23:17 -0400

    Done

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+    squares = []# your code here
+  array.each do |exp|
+    squares.push(exp ** 2)
+  end
+  squares
+##array.collect {|op| op ** 2}
+
+
 end
